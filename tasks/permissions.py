b74dc911572be9bb5e9243a381e4368c4b886012
sovushka <sovushka.sever@gmail.com>
2021-05-14 14:49:42 +0300

    fix model task, created serializers task

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -1,33 +1,3 @@
-# from rest_framework import permissions
-#
-#
-# class IsResponsable(permissions.BasePermission):
-#
-#     def has_permission(self, request, view):
-#         return (request.method in permissions.SAFE_METHODS or
-#                 request.user.is_authenticated and
-#                 request.user.is_freelancer or
-#                 request.user.is_superuser)
-#
-#     def has_object_permission(self, request, view, obj):
-#         return (request.method in permissions.SAFE_METHODS or
-#                 request.user.is_authenticated and
-#                 request.user.is_freelancer or
-#                 request.user.is_superuser)
-#
-#
-# class IsAuthor(permissions.BasePermission):
-#
-#     def has_permission(self, request, view):
-#         return (request.method in permissions.SAFE_METHODS or
-#                 request.user.is_authenticated and
-#                 request.user.is_customer)
-#
-#     def has_object_permission(self, request, view, obj):
-#         return (request.method in permissions.SAFE_METHODS and
-#                 request.user.is_customer or
-#                 obj.author == request.user)
-
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
